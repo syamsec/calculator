b557ffa5fbaf3632d97664f74308f6833e8535c5
syamsec <121809593+syamsec@users.noreply.github.com>
2023-01-23 20:44:34 +0700

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,1 +1,23 @@
+print("---------------------------")
+print("Kalkulator Sederhana Python")
+print("---------------------------\n")
+print("1. Penjumlahan \t [+]")
+print("2. Pengurangan \t [-]")
+print("3. Perkalian  \t  [*]")
+print("4. Pembagian  \t  [/]")
 
+pil = int(input("\nMasukan Pilihan Operasi : "))
+
+x= int(input("Bilangan 1 : "))
+y= int(input("Bilangan 2 : "))
+
+if pil == 1:
+   hasil = x+y
+elif pil == 2:
+   hasil = x-y
+elif pil == 3:
+   hasil = x*y
+elif pil == 4:
+   hasil = x/y
+
+print("\nHasil : %d" %hasil)
